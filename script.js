31ba5e8d49c7a87001d7e1904cfc974e8fa7bd82
Marko Jurisic <marko.jurisic86@gmail.com>
2017-04-06 00:55:41 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -9,8 +9,10 @@ function handlePlaybackRate(e) {
 		  min = 0.4,
 		  max = 2.5,
 		  height = Math.round(percent * 100) + '%',
+		  // percent * range + min
 		  playbackRate = percent * (max - min) + min;
 	
+	// set the bar's height according to the cursor position and update the rate
 	bar.style.height = height;
 	bar.textContent = playbackRate.toFixed(2) + 'x';
 	video.playbackRate = playbackRate;	
